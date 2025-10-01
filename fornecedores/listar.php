67a3d0debf33023f39e09dcb9a639c32a7ce4228
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-10-01 17:10:36 -0300

    confirm no Js

--- fornecedores/listar.php
+++ fornecedores/listar.php
@@ -51,7 +51,7 @@ echo "</pre>";
                 <td>
                     <!-- Link dinâmico, ou seja, a url/endereço utiliza parâmetro(s) e valor(es) dinâmico(s) -->
                     <a href="editar.php?id=<?=$fornecedor['id']?>">Editar</a>
-                    <a href="excluir.php?id=<?=$fornecedor['id']?>">Excluir</a>
+                    <a class="excluir" href="excluir.php?id=<?=$fornecedor['id']?>">Excluir</a>
 
                 </td>
             </tr>
@@ -60,6 +60,7 @@ echo "</pre>";
 
     </table>
 
+    <script src="../js/confirmar-exclusão.js" ></script>
 
 </body>
 </html>
